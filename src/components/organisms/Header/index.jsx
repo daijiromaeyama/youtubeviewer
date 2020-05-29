0433ba0eea6bf47efa45a762c50cedba5f29090d
daijiro.maeyama <daijiro.maeyama13@gmail.com>
2020-05-30 00:44:54 +0900

    add alias to storybook webpack

--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Typography from '../../atoms/Typography';
+import Typography from '~/components/atoms/Typography';
 
 const Root = styled.div`
   width: 100%;
